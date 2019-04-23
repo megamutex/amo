40abd5cd3f8eb4a425608adc0c5013ff61c9a93d
megamutex <123er4zapret>
2019-04-23 18:54:06 +0300

    Test commit

--- index.py
+++ index.py
@@ -1,7 +1,13 @@
+import web
+render = web.template.render('./templates/')
+urls = ("/.*", "hello")
+app = web.application(urls, globals())
+
+
 # Requests lib should be installed: http://docs.python-requests.org/en/master/user/install/#install
 import requests
 
-API_ENDPOINT = "https://dnxr7vm27d.execute-api.us-east-1.amazonaws.com/prod/GetReward"
+API_ENDPOINT = "https://dnxr7vm27d.execute-api.us-east-1.amazonaws.com/prod/GetRewardInfo"
 API_KEY = "kE2xi2OgUa7jfijmsd0jQ74aJntJwUEW2EU8LUsi" # Your API key here
 headers = {"x-api-key": API_KEY, "Content-Type": "application/json"} # Headers
 # Data to be sent to API
@@ -16,7 +22,15 @@ data = {
 r = requests.post(url = API_ENDPOINT, headers=headers, json = data)
 # extracting response text
 response = r.text
-print("headers: %s"%headers)
-print("data: %s"%data)
-print("The response is: %s"%response)
-print("The response is asda")
+
+
+
+class hello:
+    def GET(self):
+        name = 'Bob'
+        return render.index(response)
+
+if __name__ == "__main__":
+    app.run()
+
+
